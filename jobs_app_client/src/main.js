168fc8c5bb539907e86fd697d5448be73cc1193a
nova-sangeeth <novasangeeth@outlook.com>
2020-09-27 20:03:29 +0530

    added required imports and routes to the vuerouter.

--- jobs_app_client/src/main.js
+++ jobs_app_client/src/main.js
@@ -4,12 +4,38 @@ import VueAxios from 'vue-axios'
 import axios from 'axios'
 import VueRouter from 'vue-router'
 
+// component imports
+
+
+import Create from './components/Create.vue'
+import Edit from './components/Edit.vue'
+import Index from './components/Index.vue'
+
+
 Vue.use(VueRouter);
-Vue.use(VueRouter, axios);
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false
 
-const router = new VueRouter({ mode: 'history' })
+const routes = [
+  {
+    name: 'Create',
+    path: '/create',
+    component: Create
+  },
+  {
+    name: 'Edit',
+    path: '/edit',
+    component: Edit
+  },
+  {
+    name: 'Index',
+    path: '/index',
+    component: Index
+  },
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes });
 
 
 new Vue({
