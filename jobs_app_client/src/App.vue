b7268f8911ac1631002b37f00c9206c2c7889bb5
nova-sangeeth <novasangeeth@outlook.com>
2020-09-28 02:06:28 +0530

    added links for edit and index

--- jobs_app_client/src/App.vue
+++ jobs_app_client/src/App.vue
@@ -8,9 +8,23 @@
 </style>
 
 <template>
-  <transition name="fade">
-  <router-view></router-view>
-  </transition>
+  <div id="app" class="container">
+    <nav class="navbar navbar-expand-sm bg-light">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link :to="{ name: 'Create' }" class="nav-link">Add Item</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Index' }" class="nav-link">All Items</router-link>
+        </li>
+      </ul>
+    </nav>
+    <transition name="fade">
+      <div class="gap">
+        <router-view></router-view>        
+      </div>
+    </transition>
+  </div>
 </template>
 
 
