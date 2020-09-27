1479a8b8d33c4441ab0b42f355db613474bc5925
nova-sangeeth <novasangeeth@outlook.com>
2020-09-27 19:53:31 +0530

    imported the requred packages for the reading the api.

--- jobs_app_client/src/main.js
+++ jobs_app_client/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import VueRouter from 'vue-router'
 Vue.config.productionTip = false
 
 new Vue({
