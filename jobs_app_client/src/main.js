24fe10bb3e09b4c42cd1988c5f0ccfd19a3c3bac
nova-sangeeth <novasangeeth@outlook.com>
2020-09-27 19:55:20 +0530

    added the router to the main .js file.

--- jobs_app_client/src/main.js
+++ jobs_app_client/src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import VueAxios from 'vue-axios'
 import axios from 'axios'
 import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+Vue.use(VueRouter, axios);
+
 Vue.config.productionTip = false
+
+const router = new VueRouter({ mode: 'history' })
+
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
