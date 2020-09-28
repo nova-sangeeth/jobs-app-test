95311cc3821720ac10b627c0a680a7488f9a9ae9
nova-sangeeth <novasangeeth@outlook.com>
2020-09-28 16:29:27 +0530

    made changes to the deletemethod

--- jobs_app_client/src/components/Index.vue
+++ jobs_app_client/src/components/Index.vue
@@ -1,68 +1,73 @@
 
 <template>
-    <div>
-        <h1>Items</h1>
+  <div>
+    <h1>Jobs Available.</h1>
 
-        <table class="table table-hover">
-            <thead>
-            <tr>
-                <td>JOB=ID</td>
-                <td>title</td>
-                <td>description</td>
-                <td>requirements</td>
-                <td>location</td>
-                <td>timings</td>
-                <td>salary</td>
-                
-            </tr>
-            </thead>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <td>JOB=ID</td>
+          <td>title</td>
+          <td>description</td>
+          <td>requirements</td>
+          <td>location</td>
+          <td>timings</td>
+          <td>salary</td>
+        </tr>
+      </thead>
 
-            <tbody>
-                <tr v-for="item in items" :key="item.id">
-                    <td>{{ item.id }}</td>
-                    <td>{{ item.job_title }}</td>
-                    <td>{{ item.job_Description }}</td>
-                    <td>{{ item.job_requirements }}</td>
-                    <td>{{ item.job_location }}</td>
-                    <td>{{ item.job_timings }}</td>
-                    <td>{{ item.job_salary }}</td>
-                    <td><router-link :to="{name: 'Edit', params: { id: item._id }}" class="btn btn-primary">Edit</router-link></td>
-                    <td><button class="btn btn-danger"  v-on:click="deleteItem(item._id)">Delete</button></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.job_title }}</td>
+          <td>{{ item.job_Description }}</td>
+          <td>{{ item.job_requirements }}</td>
+          <td>{{ item.job_location }}</td>
+          <td>{{ item.job_timings }}</td>
+          <td>{{ item.job_salary }}</td>
+          <td>
+            <router-link
+              :to="{ name: 'Edit', params: { id: item.id } }"
+              class="btn btn-primary"
+              >Edit</router-link
+            >
+          </td>
+          <td>
+            <button class="btn btn-danger" v-on:click="deleteItem(item.id)">
+              Delete
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
 
-    export default {
-        data(){
-            return{
-                items: []
-            }
-        },
+  created: function () {
+    this.fetchItems();
+  },
 
-        created: function()
-        {
-            this.fetchItems();
-        },
-
-        methods: {
-            fetchItems()
-            {
-              let uri = 'http://localhost:8000/jobs';
-              this.axios.get(uri).then((response) => {
-                  this.items = response.data;
-                  
-              });
-            },
-            deleteItem(id)
-            {
-              let uri = 'http://localhost:8000/items/'+id;
-              this.items.splice(id, 1);
-              this.axios.get(uri);
-            }
-        }
-    }
+  methods: {
+    fetchItems() {
+      let uri = "http://localhost:8000/jobs";
+      this.axios.get(uri).then((response) => {
+        this.items = response.data;
+      });
+    },
+    deleteItem(job_id) {
+      let uri = "http://localhost:8000/jobs/" + job_id;
+      this.items.splice(job_id, 1);
+      this.axios.delete(uri);
+      console.log(this.uri + job_id);
+    },
+  },
+};
 </script>
