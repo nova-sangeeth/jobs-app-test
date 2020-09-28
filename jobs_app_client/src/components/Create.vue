c61e82555b2ca834c498843fc364c45c2b28c6bf
nova-sangeeth <novasangeeth@outlook.com>
2020-09-29 01:28:51 +0530

    removed all the divs and made changes to the uri and response.

--- jobs_app_client/src/components/Create.vue
+++ jobs_app_client/src/components/Create.vue
@@ -1,63 +1,53 @@
 <template>
   <div class="container">
-        <div class="card">
-            <div class="card-header">
-                <h3>Post A Job....</h3>
-            </div>
-            <div class="card-body">
-                <form v-on:submit.prevent="addItem">
-                    <div class="form-group">
-                        <label>Job ID</label>
-                        <input type="text" class="form-control"/>
-                    </div>
-                    <div class="form-group">
-                        <label>Job title:</label>
-                        <input type="text" class="form-control"/>
-                    </div>
-                    <div class="form-group">
-                        <label>Job desc:</label>
-                        <input type="text" class="form-control"/>
-                    </div>
-                    <div class="form-group">
-                        <label>Job requirements:</label>
-                        <input type="text" class="form-control"/>
-                    </div>
-                    <div class="form-group">
-                        <label>Job locations:</label>
-                        <input type="text" class="form-control"/>
-                    </div>
-                    <div class="form-group">
-                        <label>Job timings:</label>
-                        <input type="text" class="form-control"/>
-                    </div>
-                    <div class="form-group">
-                        <label>Job salary:</label>
-                        <input type="text" class="form-control"/>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" class="btn btn-primary" value="Add job"/>
-                    </div>
-                </form>
-            </div>
-        </div>
+    <div class="card">
+      <div class="card-header">
+        <h3>Post A Job....</h3>
+      </div>
+      <div class="card-body">
+        <form v-on:submit.prevent="addItem">
+          <label>Job title:</label>
+          <input type="text" class="form-control" />
+
+          <label>Job desc:</label>
+          <input type="text" class="form-control" />
+
+          <label>Job requirements:</label>
+          <input type="text" class="form-control" />
+
+          <label>Job locations:</label>
+          <input type="text" class="form-control" />
+
+          <label>Job timings:</label>
+          <input type="text" class="form-control" />
+
+          <label>Job salary:</label>
+          <input type="text" class="form-control" />
+
+          <input type="submit" class="btn btn-primary" value="Add job" />
+        </form>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  //   components: {
+  // name: "AddItem",
+  //   },
   data() {
-      return {
-          item: []
-      }
+    return {
+      item: {},
+    };
   },
   methods: {
-      addItem() {
-           let uri = 'http://localhost:8000/jobs/add';
-            this.axios.post(uri, this.item).then((response) => {
-                console.log(response)
-            });
-        }
-    }
-}
+    addItem() {
+      let uri = "http://localhost:8000/jobs/add";
+      this.axios
+        .post(uri, this.item)
+        .then((response) => console.log(response.data));
+    },
+  },
+};
 </script>
